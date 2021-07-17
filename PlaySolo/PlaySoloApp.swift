4452c74e4bdcf1f4f6c2d40dc6ccc0626606b074
Ankit Sharma <10382058+anklinuxboy@users.noreply.github.com>
2021-07-17 11:16:52 -0400

    Initial Commit

---
+++ PlaySolo/PlaySoloApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaySoloApp.swift
+//  PlaySolo
+//
+//  Created by Ankit Sharma on 7/17/21.
+//
+
+import SwiftUI
+
+@main
+struct PlaySoloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
